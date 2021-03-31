23f717af6df9bd0f045527b54ff47ced249abb44
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-03-31 15:17:01 -0600

    added semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Divider } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const appTitle = "To-Do App";
+
+const App = () => {
+    return <div>
+        <Container textAlign='center'>
+            <h1>{appTitle}</h1>
+        </Container>
+    </div>;
+};
 
 export default App;
